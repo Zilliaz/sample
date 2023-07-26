f0016de535b79b0aecdcad483dcce85459213767
shivaanivenkat <shivaanivenkat29@gmail.com>
2023-07-26 19:54:02 +0000

    updated build/protect to encrypt rsa as well

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -10,31 +10,64 @@ Firmware Bundle-and-Protect Tool
 import argparse
 import struct
 
-
 def protect_firmware(infile, outfile, version, message):
     # Load firmware binary from infile
     with open(infile, 'rb') as fp:
         firmware = fp.read()
-    
-    #encrypto mavuloso
-    f = open("secret_build_output.txt")
-    key = f.read(16)
-    f.close()
-    cipher = AES.new(key, AES.MODE_CBC)
-    encrypted = cipher.encrypt(pad(firmware, AES.block_size))
 
-    # Append null-terminated message to end of firmware
-    firmware_and_message = encrypted + message.encode() + b'\00'
+    # encrypting it with AES and then RSA + signing
+    keysPlural = [] # 0 -> AES, 1 -> RSA
+    with open('secret_build_output.txt', 'rb') as keyLAND:
+        keysPlural = keyLAND.reads()
 
-    # Pack version and size into two little-endian shorts
-    metadata = struct.pack('<HH', version, len(encrypted))
+    cipher = AES.new(keysPlural[0], AES.MODE_CBC)
+    iv = cipher.iv
 
-    # Append firmware and message to metadata
-    firmware_blob = metadata + firmware_and_message
+    for chunk in [firmware[i:i + 252] for i in range(0, len(firmware), 252)]:
+        if len(chunk) < 252:
+            padded = pad(chunk, 252)
+        else:
+            padded = chunk
+        
+        ct = cipher.encrypt(padded) # AES
+        h = SHA256.new(ct) # RSA
+        signature = pkcs1_15.new(keysPlural[1]).sign(h) # RSA
+        answer = h.digest() # RSA
+
+        with open(outfile, 'wb+') as outfile:
+            outfile.write(answer)
+            outfile.write(signature)
+
+    # failed attempt at encrypting into RSA
+    # key = RSA.import_key(open('secret_build_output.txt').read())
+
+    # for chunk in [firmware[i:i + 256] for i in range(0, len(firmware), 256)]:
+    #     if len(chunk) < 256:
+    #         data = pad(chunk, 256)
+    #     else:
+    #         data = chunk
+    #     print(data)
+        
+    #     h = SHA256.new(data)
+    #     signature = pkcs1_15.new(key).sign(h)
+
+    #     answer = h.digest()
+    #     with open(outfile, 'wb+') as outfile:
+    #         outfile.write(answer)
+    #         outfile.write(signature)
+
+    # # Append null-terminated message to end of firmware
+    # firmware_and_message = firmware + message.encode() + b'\00'
+
+    # # Pack version and size into two little-endian shorts
+    # metadata = struct.pack('<HH', version, len(firmware))
+
+    # # Append firmware and message to metadata
+    # firmware_blob = metadata + firmware_and_message
 
     # Write firmware blob to outfile
-    with open(outfile, 'wb+') as outfile:
-        outfile.write(firmware_blob)
+    # with open(outfile, 'wb+') as outfile:
+    #     outfile.write(firmware_blob)
 
 
 if __name__ == '__main__':
